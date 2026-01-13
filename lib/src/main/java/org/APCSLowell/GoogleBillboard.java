2c8fac8d952ce03818ad788856efad1bd86b8d27
2008woodys-netizen <2008woodys@gmail.com>
2026-01-13 18:18:39 +0000

    I did it

--- lib/src/main/java/org/APCSLowell/GoogleBillboard.java
+++ lib/src/main/java/org/APCSLowell/GoogleBillboard.java
@@ -5,11 +5,19 @@ package org.APCSLowell;
 
 public class GoogleBillboard {
     public boolean isPrime(long n) {
-        // Copy and paste your answer from CodingBat appropriatley here.
-        return false;
+        double small = Math.sqrt(n);
+        if(n <= 1)
+            return false;
+        for(int i = 2; i<=small; i++)
+            if(n%i==0)
+            return false;
+        return true;
     }
     public long firstPrimeNumberIn(String n) {
-        // Write your code here.
+        for(int i = 0; i<=n.length()-10; i++)
+            if(isPrime(Long.parseLong(n.substring(i,i+10))))
+                return Long.parseLong(n.substring(i,i+10));
         return -1;
     }
+
 }
